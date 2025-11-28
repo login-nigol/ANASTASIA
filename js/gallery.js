064b9666e18a1a050638b7d881d0e1b007797c17
Vadim <vadim.antipov@gmx.de>
2025-11-28 13:35:50 +0100

    feat: add mobile adaptation for main page

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 // Навигация назад
 document.querySelector(".back-btn").addEventListener("click", function () {
-  window.location.href = "index.html";
+  window.location.href = "main.html";
 });
 
 // Модальное окно
